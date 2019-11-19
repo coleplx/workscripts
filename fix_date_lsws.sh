3376fec80799bfe4fb7d85fd669c2cf3bac629db
coleplx <paulo@aliancaproject.com>
2019-11-19 12:38:37 -0300

    Update fix_date_lsws.sh

--- fix_date_lsws.sh
+++ fix_date_lsws.sh
@@ -7,10 +7,7 @@ for i in $(find /usr/bin/ -type l -iname lsphp*); do
         DEBIAN_FRONTEND=noninteractive apt-get install -q -y $VERSAO-dev
         wget https://pecl.php.net/get/timezonedb-2019.3.tgz -O /usr/local/src/timezonedb.tgz
         tar -xf /usr/local/src/timezonedb.tgz -C /usr/local/src/
-        cd /usr/local/src/timezonedb-2019.3/; /usr/local/lsws/lsphp56/bin/phpize
-        ./configure --with-php-config=/usr/local/lsws/lsphp56/bin/php-config
-        make
-        make install
+        cd /usr/local/src/timezonedb-2019.3/; /usr/local/lsws/lsphp56/bin/phpize && ./configure --with-php-config=/usr/local/lsws/lsphp56/bin/php-config && make && make install
     else
         DEBIAN_FRONTEND=noninteractive apt-get install -q -y $VERSAO-pear $VERSAO-dev
         /usr/local/lsws/$VERSAO/bin/pecl install timezonedb
