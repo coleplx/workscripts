012abf306c85c44ae269fe6b7d6ca1438805ff20
coleplx <paulo@aliancaproject.com>
2019-11-19 12:35:41 -0300

    Update fix_date_lsws.sh

--- fix_date_lsws.sh
+++ fix_date_lsws.sh
@@ -3,10 +3,23 @@
 for i in $(find /usr/bin/ -type l -iname lsphp*); do
     VERSAO=$(echo $i | cut -d'/' -f4 | sed -e 's/\.//')
     VERSAO_SHORT=$(echo $i | egrep -o [0-9.]+)
-    DEBIAN_FRONTEND=noninteractive apt-get install -q -y $VERSAO-pear $VERSAO-dev
-    /usr/local/lsws/$VERSAO/bin/pecl install timezonedb
+    if [ $VERSAO_SHORT = "5.6" ]; then
+        DEBIAN_FRONTEND=noninteractive apt-get install -q -y $VERSAO-dev
+        wget https://pecl.php.net/get/timezonedb-2019.3.tgz -O /usr/local/src/timezonedb.tgz
+        tar -xf /usr/local/src/timezonedb.tgz -C /usr/local/src/
+        cd /usr/local/src/timezonedb-2019.3/; /usr/local/lsws/lsphp56/bin/phpize
+        make
+        make install
+    else
+        DEBIAN_FRONTEND=noninteractive apt-get install -q -y $VERSAO-pear $VERSAO-dev
+        /usr/local/lsws/$VERSAO/bin/pecl install timezonedb
+    fi
     if [ $? == 0 ]; then
-        echo 'extension=timezonedb.so' > /usr/local/lsws/$VERSAO/etc/php/$VERSAO_SHORT/mods-available/timezonedb.ini
+        if [ $VERSAO_SHORT = "5.6" ]; then
+            echo 'extension=timezonedb.so' > /usr/local/lsws/lsphp56/etc/conf.d/timezonedb.ini
+        else
+            echo 'extension=timezonedb.so' > /usr/local/lsws/$VERSAO/etc/php/$VERSAO_SHORT/mods-available/timezonedb.ini
+        fi
     fi
 done
 
