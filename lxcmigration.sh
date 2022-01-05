bb00c7b70f233265833372a79ea8bcc9db13c869
Paulo Paracatu <paulo@aliancaproject.com>
2022-01-05 11:27:37 -0300

    Update lxcmigration.sh

--- lxcmigration.sh
+++ lxcmigration.sh
@@ -6,12 +6,12 @@ DISK_USAGE=$(zfs list)
 for container in $(echo "$RUNNING_CONTAINERS" | grep -v '\-staging\-'); do
   CONTAINER_DISK=$(echo "$DISK_USAGE" | grep $container | awk '{print $2}')
   # If disk usage is less than 1gb, we can consider it = 0 (easier to calculate)
-  if [ $(echo $CONTAINER_DISK | egrep (M|K) ]; then
+  if [ $(echo $CONTAINER_DISK | egrep "(M|K)") ]; then
     CONTAINER_DISK=0
   else
-    CONTAINER_DISK=$(echo "$DISK_USAGE" | grep $container | awk '{print $2}' | egrep -o [0-9.]+ | cut -d'.' -f)
+    CONTAINER_DISK=$(echo "$CONTAINER_DISK" | egrep -o [0-9.]+ | cut -d'.' -f1)
   fi
-  
+
   if [ $CONTAINER_DISK -lt 7 ]; then
     CONTAINER_MEMORY=$(cat /sys/fs/cgroup/memory/lxc.payload.${container}/memory.usage_in_bytes | awk '{print $1 / 1024 / 1024 / 1024}')
     echo $container $CONTAINER_MEMORY
