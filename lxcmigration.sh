584af6b440ea8feea42d3131221f4afe8adb754f
Paulo Paracatu <paulo@aliancaproject.com>
2022-01-11 14:37:32 -0300

    Update lxcmigration.sh

--- lxcmigration.sh
+++ lxcmigration.sh
@@ -1,19 +1,41 @@
 #!/bin/bash
 # ./lxcmigration.sh
+# v0.1 - Initial Version
+# v0.2 - Fix problem with small containers (<1gb) being excluded from the listing
+# v0.3 - Drop zfs list in favor of /kinsta/main.conf
+# v0.4 - Use readarray to avoid "column: line too long" error
+# v1.0 - Check staging disk usage too, exclude site if staging usage is bigger than 7 GiB
+#
 RUNNING_CONTAINERS=$(ps aux | grep 'lxc monito[r]' | rev | cut -d' ' -f1 | rev)
-DISK_USAGE=$(zfs list)
 
-for container in $(echo "$RUNNING_CONTAINERS" | grep -v '\-staging\-'); do
-  CONTAINER_DISK=$(echo "$DISK_USAGE" | grep $container | awk '{print $2}')
-  # If disk usage is less than 1gb, we can consider it = 0 (easier to calculate)
-  if [ $(echo $CONTAINER_DISK | egrep "(M|K)") ]; then
-    CONTAINER_DISK=0
+
+cont_list=$(for container in $(echo "$RUNNING_CONTAINERS" | grep -v '\-staging\-' ); do
+  SITE_NAME=$(echo $container | cut -d'-' -f2)
+  # Check if there is a staging running
+  STAGING_NAME=$(echo "$RUNNING_CONTAINERS" | grep "\-staging\-$SITE_NAME")
+  if [ $? == 0 ]; then
+    STAGING_RUNNING=1
+    STAGING_DISK=$(lxc exec $STAGING_NAME -- cat /kinsta/main.conf | grep ^disk_usage_full | cut -d'=' -f2)
+    STAGING_DISK_GIB=$(echo "$STAGING_DISK" | awk '{printf "%0.2f", $1 / 1024 / 1024 /1024}')
   else
-    CONTAINER_DISK=$(echo "$CONTAINER_DISK" | egrep -o [0-9.]+ | cut -d'.' -f1)
+   STAGING_RUNNING=0
   fi
 
-  if [ $CONTAINER_DISK -lt 7 ]; then
+  CONTAINER_DISK=$(lxc exec $container -- cat /kinsta/main.conf | grep ^disk_usage_full | cut -d'=' -f2)
+  CONTAINER_DISK_GIB=$(echo "$CONTAINER_DISK" | awk '{printf "%0.2f", $1 / 1024 / 1024 /1024}')
+
+  # 7516192768 bytes = 7 GiB
+  if [ $CONTAINER_DISK -lt 7516192768 ]; then
     CONTAINER_MEMORY=$(cat /sys/fs/cgroup/memory/lxc.payload.${container}/memory.usage_in_bytes | awk '{print $1 / 1024 / 1024 / 1024}')
-    echo $container $CONTAINER_MEMORY
+    if [ $STAGING_RUNNING == 1 ]; then
+      if [ $STAGING_DISK -lt 7516192768 ]; then
+        echo "$container $CONTAINER_MEMORY $CONTAINER_DISK_GIB $STAGING_DISK_GIB"
+      fi
+    else
+      echo "$container $CONTAINER_MEMORY $CONTAINER_DISK_GIB"
+    fi
   fi
-done | sort -nr -k2 | head -n 10
+done | sort -nr -k2 )
+
+readarray -t ARR < <(printf "CONTAINER 'MEMORY' 'LIVE-Disk-GiB' 'STAGING-Disk-GiB' \n$cont_list")
+printf '%s\n' "${ARR[@]}" | column  -t
