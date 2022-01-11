0701f3f8885a52162f94dfbd9af63225c6a30191
Paulo Paracatu <paulo@aliancaproject.com>
2022-01-11 16:01:57 -0300

    Update lxddt.sh

--- lxddt.sh
+++ lxddt.sh
@@ -5,8 +5,8 @@ MAX_RUNNING_LXC=$(grep LXD_META_NUMBER_OF_RUNNING_CONTAINERS /var/log/syslog{.1,
 CURRENT_RUNNING_LXC=$(ps aux | grep '[l]xc monitor' | wc -l)
 
 
-LAST_LIVE_CONTAINER_NAME=$(cat lxd.log | grep 'Started container' | grep -v '\-staging\-' | tail -n1 | grep -o instance=.* | cut -d'=' -f2 | cut -d' ' -f1)
-LAST_LIVE_CONTAINER_TIME=$(cat lxd.log | grep 'Started container' | grep "$LAST_LIVE_CONTAINER_NAME" | tail -n1 | cut -d' ' -f1 | egrep -o '[0-9]+:[0-9]+')
+LAST_LIVE_CONTAINER_NAME=$(cat /var/snap/lxd/common/lxd/logs/lxd.log | grep 'Started container' | grep -v '\-staging\-' | tail -n1 | grep -o instance=.* | cut -d'=' -f2 | cut -d' ' -f1)
+LAST_LIVE_CONTAINER_TIME=$(cat /var/snap/lxd/common/lxd/logs/lxd.log | grep 'Started container' | grep "$LAST_LIVE_CONTAINER_NAME" | tail -n1 | cut -d' ' -f1 | egrep -o '[0-9]+:[0-9]+')
 
 echo ""
 echo "Time now: $(date '+%H:%M') UTC"
