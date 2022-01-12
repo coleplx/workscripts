385724015209f7de0370bfba1b0fbad094d51238
Paulo Paracatu <paulo@aliancaproject.com>
2022-01-12 16:57:46 -0300

    Update logsize-fix.sh

--- logsize-fix.sh
+++ logsize-fix.sh
@@ -6,7 +6,7 @@ NEW_LOG_SIZE=$(echo $LOG_SIZE 10 | awk '{print $1 + $2}')
 SITE_PATH=$(cat /etc/nginx/sites/*.conf  |grep 'root /www/' | head -n1 | cut -d'/' -f3)
 
 # Change innodb_log_file_size
-sed -i 's/innodb_log_file_size = 50M/innodb_log_file_size = 60M/' /etc/mysql/my.cnf
+sed -i "s/innodb_log_file_size = 50M/innodb_log_file_size = ${NEW_LOG_SIZE}M/" /etc/mysql/my.cnf
 sed -i 's/#innodb_log_file_size/innodb_log_file_size/' /etc/mysql/my.cnf
 mysql --defaults-file=/etc/mysql/debian.cnf -e 'SET GLOBAL innodb_fast_shutdown = 0;'
 systemctl stop mariadb
