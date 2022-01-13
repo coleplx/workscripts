61bd2eb9e7bd99b168f7370929db8cf5897f9c05
Paulo Paracatu <paulo@aliancaproject.com>
2022-01-13 08:59:27 -0300

    Update logsize-fix.sh

--- logsize-fix.sh
+++ logsize-fix.sh
@@ -6,12 +6,18 @@ NEW_LOG_SIZE=$(echo $LOG_SIZE 10 | awk '{print $1 + $2}')
 SITE_PATH=$(cat /etc/nginx/sites/*.conf  |grep 'root /www/' | head -n1 | cut -d'/' -f3)
 
 # Change innodb_log_file_size
-sed -i "s/innodb_log_file_size = 50M/innodb_log_file_size = ${NEW_LOG_SIZE}M/" /etc/mysql/my.cnf
-sed -i 's/#innodb_log_file_size/innodb_log_file_size/' /etc/mysql/my.cnf
+sed -i "s/innodb_log_file_size.*/innodb_log_file_size = ${NEW_LOG_SIZE}M/g" /etc/mysql/my.cnf
+sed -i 's/#innodb_log_file_size/innodb_log_file_size/g' /etc/mysql/my.cnf
 mysql --defaults-file=/etc/mysql/debian.cnf -e 'SET GLOBAL innodb_fast_shutdown = 0;'
-systemctl stop mariadb
+
+# Make sure mysql isn't running
+systemctl stop mariadb mysql
+kill -9 $(pgrep -f mysql); kill -9 $(pgrep -f mariadb)
+
+# Backup the ib_logfile
 mv /var/lib/mysql/ib_logfile* /www/$SITE_PATH/private/
 
-# fix the legacy mysql systemd wrapper
+# fix the legacy mysql systemd wrapper and start mysql again
+systemctl disable mysql
 systemctl enable mariadb
 systemctl start mariadb
