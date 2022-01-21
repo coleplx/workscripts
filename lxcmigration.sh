497be009a5d64a1b496c23b522dc970780b01d5c
Paulo Paracatu <paulo@aliancaproject.com>
2022-01-21 17:13:54 -0300

    Update lxcmigration.sh

--- lxcmigration.sh
+++ lxcmigration.sh
@@ -6,6 +6,7 @@
 # v0.4 - Use readarray to avoid "column: line too long" error
 # v1.0 - Check staging disk usage too, exclude site if staging usage is bigger than 7 GiB
 # v1.1 - Set disk = 0 when variable is empty
+# v1.2 - Dropped lxc exc in favor of nsenter
 #
 RUNNING_CONTAINERS=$(ps aux | grep 'lxc monito[r]' | rev | cut -d' ' -f1 | rev)
 
@@ -16,14 +17,16 @@ cont_list=$(for container in $(echo "$RUNNING_CONTAINERS" | grep -v '\-staging\-
   STAGING_NAME=$(echo "$RUNNING_CONTAINERS" | grep "\-staging\-$SITE_NAME")
   if [ $? == 0 ]; then
     STAGING_RUNNING=1
-    STAGING_DISK=$(lxc exec $STAGING_NAME -- cat /kinsta/main.conf | grep ^disk_usage_full | cut -d'=' -f2)
+    # Access the LXD mount namespace
+    STAGING_DISK=$(nsenter -t $(cat /var/snap/lxd/common/lxd.pid) -m cat /var/snap/lxd/common/lxd/storage-pools/default/containers/$STAGING_NAME/rootfs/kinsta/main.conf | grep ^disk_usage_full | cut -d'=' -f2)
+    #STAGING_DISK=$(lxc exec $STAGING_NAME -- cat /kinsta/main.conf | grep ^disk_usage_full | cut -d'=' -f2)
     if [ "$STAGING_DISK" == "" ]; then STAGING_DISK=0; fi
     STAGING_DISK_GIB=$(echo "$STAGING_DISK" | awk '{printf "%0.2f", $1 / 1024 / 1024 /1024}')
   else
    STAGING_RUNNING=0
   fi
 
-  CONTAINER_DISK=$(lxc exec $container -- cat /kinsta/main.conf | grep ^disk_usage_full | cut -d'=' -f2)
+  CONTAINER_DISK=$(nsenter -t $(cat /var/snap/lxd/common/lxd.pid) -m cat /var/snap/lxd/common/lxd/storage-pools/default/containers/$container/rootfs/kinsta/main.conf | grep ^disk_usage_full | cut -d'=' -f2)
   if [ "$CONTAINER_DISK" == "" ]; then CONTAINER_DISK=0; fi
   CONTAINER_DISK_GIB=$(echo "$CONTAINER_DISK" | awk '{printf "%0.2f", $1 / 1024 / 1024 /1024}')
 
